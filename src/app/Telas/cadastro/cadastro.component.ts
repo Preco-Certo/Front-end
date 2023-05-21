e607b375e965298388504702581cb574f601e254
lucastamura <lucas10.tamura@gmail.com>
2023-05-21 00:56:42 -0300

    fix: tela de cadastro

--- src/app/Telas/cadastro/cadastro.component.ts
+++ src/app/Telas/cadastro/cadastro.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent {
+  title = 'Cadastre-se';
+  subtitle = 'Insira os dados de seu negócios para nós ajudar a entender quais são suas necessidades';
+
+  // placeholder
+
+  cnpj = '12.345.678/0001-91';
+  razao = 'Associados Alimentos LTDA';
+  fantasia = 'Restaurante Cantina';
+  cep = '17.500-500';
+  endereco = 'Avenida Sampaio Vidal';
+  numero = '123';
+  bairro = 'Centro';
+  cidade = 'São Paulo';
+  uf = 'SP';
+  complemento = 'Galeria de Lojas';
+  celular = '(14) 99876-5432';
+  email = 'email@email.com';
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Telas/login/login.component';
 import { HomeComponent } from './Telas/home/home.component';
+import { CadastroComponent } from './Telas/cadastro/cadastro.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent }
 ];
 
 @NgModule({
