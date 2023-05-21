88a946c1aa4b0905de4d84920d5e95644746e70b
lucastamura <lucas10.tamura@gmail.com>
2023-05-20 23:16:08 -0300

    add: tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Telas/login/login.component';
+import { HeaderComponent } from './Componentes/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
