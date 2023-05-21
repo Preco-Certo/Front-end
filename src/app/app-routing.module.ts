c10ab3adfd6056007b4fd075715b7daf79419978
lucastamura <lucas10.tamura@gmail.com>
2023-05-20 23:52:17 -0300

    add: home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Telas/login/login.component';
+import { HomeComponent } from './Telas/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
